ae27cad32fd09d1dc77c9379b5845156c97ed6bd
momay-2332 <matineehutti@gmail.com>
2020-03-02 15:36:05 +0700

    finish 2March

---
+++ lib/utility/my_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyStyle{
+   // Field
+  Color darkColor = Colors.purple.shade700;
+  Color primaryColor = Colors.yellow;
+
+   //Method
+   MyStyle();
+}
