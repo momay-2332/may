7b8ebc9668bc5a33380d6a6063abad5f73c9a976
momay-2332 <momay.hut@gmail.com>
2020-03-10 15:25:15 +0700

    ProjectSuscess

--- lib/container/show_list_product.dart
+++ lib/container/show_list_product.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:may/models/product_model.dart';
+import 'package:may/utility/my_style.dart';
+import 'package:may/widget/detail.dart';
 
 class ShowListProduct extends StatefulWidget {
   @override
@@ -7,34 +10,115 @@ class ShowListProduct extends StatefulWidget {
 }
 
 class _ShowListProductState extends State<ShowListProduct> {
+  // Field
+  String urlAPI = 'http://jsonplaceholder.typicode.com/photos';
+  List<ProductModel> productModels = List();
+  ScrollController scrollController = ScrollController();
+  int sizeListView = 10;
 
-
-  //field
-    String urlAPI = 'https://jsonplaceholder.typicode.com/photos';
-
-  //Method
+  // Method
   @override
   void initState() {
     super.initState();
     readJSON();
-
+    setupController();
   }
 
-Future<void> readJSON()async{
-  try {
-
-    Response response = await Dio().get(urlAPI);
-    print('response = $response');
-    
-  } catch (e) {
+  void setupController() {
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        print('This is End');
+        setState(() {
+          sizeListView += 10;
+        });
+      }
+    });
   }
 
-}
+  Future<void> readJSON() async {
+    try {
+      Response response = await Dio().get(urlAPI);
+      print('response = $response');
 
+      for (var map in response.data) {
+        ProductModel model = ProductModel.fromJson(map);
+        setState(() {
+          productModels.add(model);
+        });
+      }
+    } catch (e) {}
+  }
 
+  Widget showProcess() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget showListView() {
+    return ListView.builder(
+        controller: scrollController,
+        itemCount: sizeListView,
+        itemBuilder: (BuildContext buildContext, int index) {
+          return GestureDetector(
+            onTap: () {
+              MaterialPageRoute route = MaterialPageRoute(
+                builder: (value) => Detail(
+                  model: productModels[index],
+                ),
+              );
+              Navigator.of(context).push(route);
+            },
+            child: Row(
+              children: <Widget>[
+                showPic(index),
+                showText(index),
+              ],
+            ),
+          );
+        });
+  }
+
+  Widget showPic(int index) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      width: MediaQuery.of(context).size.width * 0.5,
+      child: Image.network(productModels[index].url),
+    );
+  }
+
+  Widget showText(int index) {
+    return Container(
+      padding: EdgeInsets.only(top: 16.0, right: 16.0, bottom: 16.0),
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.width * 0.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          showId(index),
+          showTitle(index),
+        ],
+      ),
+    );
+  }
+
+  Widget showId(int index) {
+    return Text(
+      productModels[index].id.toString(),
+      style: TextStyle(
+          fontSize: 70.0,
+          fontWeight: FontWeight.bold,
+          color: MyStyle().darkColor),
+    );
+  }
+
+  Widget showTitle(int index) {
+    return Text(productModels[index].title);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text('showlist');
+    return productModels.length == 0 ? showProcess() : showListView();
   }
 }
--- lib/container/show_map.dart
+++ lib/container/show_map.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ShowMap extends StatefulWidget {
   @override
   _ShowMapState createState() => _ShowMapState();
 }
 
-class _ShowMapState extends State<ShowMap > {
+class _ShowMapState extends State<ShowMap> {
+  //field
+  static LatLng gloCenter = LatLng(13.881852, 100.505421);
+  CameraPosition cameraPosition = CameraPosition(
+    target: gloCenter,
+    zoom: 16.0,
+  );
+
+  //Method
+  Marker gloMarker() {
+    return Marker(
+        infoWindow: InfoWindow(
+            title: 'สำนักงานสลากกินแบ่งรัฐบาล',
+            snippet:
+                '359 ถนนสนามบินน้ำ ตำบลท่าทราย อำเภอเมืองนนทบุรี จังหวัดนนทบุรี'),
+        markerId: MarkerId('glo'),
+        position: gloCenter);
+  }
+
+  Set<Marker> setMarker() {
+    return <Marker>[
+      gloMarker(),
+    ].toSet();
+  }
+
+  Widget showGoogleMap() {
+    return GoogleMap(
+      markers: setMarker(),
+      initialCameraPosition: cameraPosition,
+      mapType: MapType.normal,
+      onMapCreated: (GoogleMapController controller) {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('showMap');
+    return showGoogleMap();
   }
 }
